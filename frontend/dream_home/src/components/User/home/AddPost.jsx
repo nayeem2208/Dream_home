c7067b5c8e055b61cfea085c09fbcb2ebe7a6cd9
Nayeem <nayeem2281998@gmail.com>
2023-09-21 10:21:22 +0530

    post designing setting

--- frontend/dream_home/src/components/User/home/AddPost.jsx
+++ frontend/dream_home/src/components/User/home/AddPost.jsx
@@ -1,18 +1,110 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 function AddPost() {
+  let [modalVisible, setModalVisible] = useState(false);
+  let [heading,setheading]=useState('')
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
   return (
     <div>
       <div className=" flex flex-col justify-center items-center">
         <button
           type="button"
+          onClick={toggleModal}
           className="h-9 mt-6 px-3 w-2/4 items-center text-neutral-800 my-4  bg-gradient-to-r from-teal-100 via-teal-200 to-teal-150 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-200 font-medium rounded-lg   "
         >
-          Add a Post
+          <b>Add a Post</b>
         </button>
 
-        
+        {modalVisible && (
+          <div>
+            <div
+              className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-70 z-40"
+              onClick={toggleModal}
+            ></div>
+            <div
+              id="defaultModal"
+              className="fixed flex items-center justify-center top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+            >
+              <div className="relative w-full max-w-2xl max-h-full">
+                <div className="relative bg-white rounded-lg shadow text-neutral-800 my-4  bg-gradient-to-r from-teal-400 via-teal-300 to-teal-150 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-500 dark:focus:ring-teal-300">
+                  <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-100">
+                    <h3 className="text-xl font-bold text-gray-900 ">
+                      Create post
+                    </h3>
+                    <button
+                      type="button"
+                      onClick={toggleModal}
+                      className="text-gray-900 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-hide="defaultModal"
+                    >
+                      <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 14"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                        />
+                      </svg>
+                      <span className="sr-only">Close modal</span>
+                    </button>
+                  </div>
+
+                  <div className="p-6 space-y-3 flex flex-col">
+                    <label htmlFor="Heading">Heading</label>
+                    <input type="text" className="rounded-lg" onChange={(e)=>setheading(e.target.value)} />
+                    <label htmlFor="description">Description</label>
+                    <textarea type="text" className="rounded-lg" />
+                    <label htmlFor="service">Service</label>
+                    <input type="text" className="rounded-lg" />
+                    <label htmlFor="Media">Media</label>
+                    <input
+                      type="file"
+                      className="hidden" // Hide the default file input
+                      id="fileInput" // Add an ID for easier styling
+                      accept=".jpg, .jpeg, .png" // Specify accepted file types
+                    />
+
+                    <label
+                      htmlFor="fileInput" // Associate the label with the file input
+                      className="rounded-lg bg-mainColor py-2 px-4 text-white cursor-pointer hover:bg-hoverColor transition duration-300"
+                    >
+                      Choose a File
+                    </label>
+                  </div>
+
+                  <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-100">
+                    <button
+                      data-modal-hide="defaultModal"
+                      type="button"
+                      className="text-white font-semibold  bg-mainColor hover:bg-mainColor-400 focus:ring-4 focus:outline-none focus:ring-mainColor-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-mainColor-600 dark:hover:bg-mainColor-700 dark:focus:ring-mainColor-800"
+                    >
+                      Post
+                    </button>
+                    <button
+                      data-modal-hide="defaultModal"
+                      type="button"
+                      onClick={toggleModal}
+                      className="text-gray-500 font-semibold bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/dream_home/src/components/User/home/HomePost.jsx
+++ frontend/dream_home/src/components/User/home/HomePost.jsx
@@ -1,59 +1,263 @@
-import image from '../../../assets/armchair-green-living-room-with-copy-space.jpg'
-import {AiOutlineLike} from 'react-icons/ai'
-import React from 'react'
+import image from "../../../assets/armchair-green-living-room-with-copy-space.jpg";
+import { AiOutlineLike } from "react-icons/ai"; // Example of an outline-style Like icon
+import { AiTwotoneLike } from "react-icons/ai"; // Example of a two-tone style Like icon
+import { BiComment } from "react-icons/bi"; // Comment icon
+import { IoSendOutline } from "react-icons/io5"; // Send icon
+import { GrFormNext, GrFormPrevious } from "react-icons/gr"; // Next and Previous icons from the Gr (React Icons Gr library)
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState } from "react";
 
 function HomePost() {
+  let [showMore,setShowmore]=useState(true)
+  let [liked,setLiked]=useState(false)
+  let [like,setLike]=useState(150)
+  
+
+  
+  let showmoreToggle=()=>{
+    setShowmore(!showMore)
+  }
+
+  let likeToggle=()=>{
+    setLiked(!liked)
+    if(liked){
+      setLike(like-1)
+    }else setLike(like+1)
+  }
+
+  console.log(like)
+
   return (
-    <div>
-        <div class="max-w-3xl bg-white border  rounded-lg shadow dark:bg-gray-50 dark:border-gray-300 my-4 w-screen ">
+    <div className="min-h-screen flex flex-col items-center justify-center ">
+      <div class="max-w-5xl items-center bg-white border  rounded-lg shadow dark:bg-gray-50 dark:border-gray-300 my-4 w-screen ">
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
           
-          <div class="p-5">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                Noteworthy technology acquisitions 2021
-              </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-700">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.Lorem Ipsum is simply dummy text
+            of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book.Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book.Lorem
+            Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book
+          </p>
+          <button
+            onClick={showmoreToggle}
+            class="inline-flex items-center px-3  text-sm font-medium text-center   "
+          >
+            Show more
+            <svg
+              class="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
             >
-              Show more
-              <svg
-                class="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </button>
+        </div>
+        <a href="#">
+          <div className="relative overflow-hidden h-96">
+            <a href="#">
+              <div className="relative overflow-hidden h-96">
+                <Carousel className="px-2">
+                  <div>
+                    <img src={image} alt="Image 1" />
+                  </div>
+                  <div>
+                    <img src={image} alt="Image 2" />
+                  </div>
+                  <div>
+                    <img src={image} alt="Image 3" />
+                  </div>
+                </Carousel>
+              </div>
             </a>
           </div>
-          <a href="#">
-            <div className='relative  overflow-hidden h-96'>
-            <img
-              class="p-3 w-full h-full object-cover"
-              src={image}
-              alt=""
+        </a>
+
+        <div className="bg-grey-300 h-12 justify-items-stretch mt-4">
+          <div className="flex ">
+            {liked?(<AiTwotoneLike onClick={likeToggle} className=" ml-8 mt-1 w-5 h-5 "/>):(
+            <AiOutlineLike onClick={likeToggle} className=" ml-8 mt-1 w-5 h-5 " />)}
+            <p className="mr-6">{like}</p>
+            <BiComment className="mx-3 mt-1 w-5 h-5" />
+            <p className="mr-6">115</p>
+            <input
+              type="text"
+              className="border ml-4 rounded-lg w-2/4 text-center placeholder-center"
+              placeholder="Comment here"
             />
-            </div>
-          </a>
-          <div className='bg-grey-300 h-24'>
-            
+            <IoSendOutline className="ml-8 mt-1 w-5 h-5" />
           </div>
         </div>
+      </div>
+
+      {/* <div class="max-w-5xl items-center bg-white border  rounded-lg shadow dark:bg-gray-50 dark:border-gray-300 my-4 w-screen ">
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.Lorem Ipsum is simply dummy text
+            of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Show more
+            <svg
+              class="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+        <a href="#">
+          <div className="relative overflow-hidden h-96">
+            <a href="#">
+              <div className="relative overflow-hidden h-96">
+                <Carousel className="px-2">
+                  <div>
+                    <img src={image} alt="Image 1" />
+                  </div>
+                  <div>
+                    <img src={image} alt="Image 2" />
+                  </div>
+                  <div>
+                    <img src={image} alt="Image 3" />
+                  </div>
+                </Carousel>
+              </div>
+            </a>
+          </div>
+        </a>
+
+        <div className="bg-grey-300 h-12 justify-items-stretch mt-4">
+          <div className="flex ">
+            <AiOutlineLike className=" ml-8 mt-1 w-5 h-5 " />
+            <p className="mr-6">150</p>
+            <BiComment className="mx-3 mt-1 w-5 h-5" />
+            <p className="mr-6">115</p>
+            <input
+              type="text"
+              className="border ml-4 rounded-lg w-2/4 text-center placeholder-center"
+              placeholder="Comment here"
+            />
+            <IoSendOutline className="ml-8 mt-1 w-5 h-5" />
+          </div>
+        </div>
+      </div>
+      <div class="max-w-5xl items-center bg-white border  rounded-lg shadow dark:bg-gray-50 dark:border-gray-300 my-4 w-screen ">
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-700">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Show more
+            <svg
+              class="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+        <a href="#">
+          <div className="relative overflow-hidden h-96">
+            <a href="#">
+              <div className="relative overflow-hidden h-96">
+                <Carousel className="px-2">
+                  <div>
+                    <img src={image} alt="Image 1" />
+                  </div>
+                  <div>
+                    <img src={image} alt="Image 2" />
+                  </div>
+                  <div>
+                    <img src={image} alt="Image 3" />
+                  </div>
+                </Carousel>
+              </div>
+            </a>
+          </div>
+        </a>
+
+        <div className="bg-grey-300 h-12 justify-items-stretch mt-4">
+          <div className="flex ">
+            <AiOutlineLike className=" ml-8 mt-1 w-5 h-5 " />
+            <p className="mr-6">150</p>
+            <BiComment className="mx-3 mt-1 w-5 h-5" />
+            <p className="mr-6">115</p>
+            <input
+              type="text"
+              className="border ml-4 rounded-lg w-2/4 text-center placeholder-center"
+              placeholder="Comment here"
+            />
+            <IoSendOutline className="ml-8 mt-1 w-5 h-5" />
+          </div>
+        </div>
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default HomePost
+export default HomePost;
