b3524d24858678629465fef1b9b2fce54505cfc5
Nayeem <nayeem2281998@gmail.com>
2023-10-09 19:46:16 +0530

    changed let into const

---
+++ backend/controllers/UserAuthController.js
@@ -0,0 +1,200 @@
+import asyncHandler from "express-async-handler";
+import usermodel from "../modals/userModal.js";
+import generateToken from "../utils/userJwt.js";
+import sendresetmail from "../utils/nodeMailer.js";
+import jwt from "jsonwebtoken";
+
+const registerUser = asyncHandler(async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
+    const userExist = await usermodel.findOne({ email });
+    const usernameExist = await usermodel.findOne({ username });
+    if (userExist) {
+      res.status(400).json("Email already exists");
+    }
+    if (usernameExist) {
+      res.status(400).json("Please Use unique usename");
+    }
+    const user = await usermodel.create({
+      username,
+      email,
+      phone,
+      password,
+      profilePic: "file_1695748280782.png",
+    });
+    if (user) {
+      const token = generateToken(res, user._id);
+      res.status(200).json({
+        id: user._id,
+        name: user.username,
+        email: user.email,
+        image: user.profilePic,
+        token,
+      });
+    }
+  } catch (err) {
+    res.status(400).json({ err: "Email already exist" });
+  }
+});
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await usermodel.findOne({ email });
+    if (user) {
+      if (await user.matchpassword(password)) {
+        if (user.isBlocked) {
+          res.status(400).json({ error: "User is blocked" });
+        } else {
+          const token = generateToken(res, user._id);
+          res.status(200).json({
+            id: user._id,
+            name: user.username,
+            email: user.email,
+            image: user.profilePic,
+            token,
+          });
+        }
+      } else {
+        res.status(400).json({ error: "Wrong password" });
+      }
+    } else {
+      res.status(400).json({ error: "Wrong email" });
+    }
+  } catch (err) {
+    res.status(400).json({ error: "invalid " });
+  }
+};
+
+const check = (req, res) => {
+  res.status(200).json("Its workingggggggggggggggg");
+};
+
+const forgotpassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await usermodel.findOne({ email });
+    const token1 = Math.floor(100000 + Math.random() * 900000).toString();
+    const otpExpiration = new Date(Date.now() + 1 * 120 * 1000);
+    if (user) {
+      user.token = token1;
+      user.otpExpiration = otpExpiration;
+      await user.save();
+      sendresetmail(user.username, email, user.token);
+      res.status(200).json("its working");
+    } else {
+      res.status(400).json("User not found");
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+const verifyOtp = async (req, res) => {
+  try {
+    const { state, otp } = req.body;
+
+    const user = await usermodel.findOne({ email: state });
+    if (user.token == otp) {
+      const currentTime = new Date();
+      if (currentTime <= user.otpExpiration) {
+        res.status(200).json("otp is matched");
+      } else {
+        res.status(200).json("Otp expired");
+      }
+    } else {
+      res.status(400).json("Otp not matched");
+    }
+  } catch (err) {
+    res.status(400).json({ error });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const { state, password } = req.body;
+    const user = await usermodel.findOne({ email: state });
+    user.password = password;
+    await user.save();
+    res.status(200).json("reset password is working");
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+const googleAuth = async (req, res) => {
+  try {
+    const token = req.body.credentialResponse.credential;
+    const decode = jwt.decode(token);
+    const { name, email, sub } = decode;
+    const userExists = await usermodel.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ error: "User already exists" });
+    }
+
+    const username = "";
+    for (let i = 0; i < email.length; i++) {
+      if (email[i] == "@") {
+        break;
+      } else {
+        username += email[i];
+      }
+    }
+
+    const user = await usermodel.create({
+      username: username,
+      name: name,
+      email,
+      sub,
+      profilePic: "file_1695748280782.png",
+    });
+    if (user) {
+      const token = generateToken(res, user._id);
+      res.status(200).json({
+        id: user._id,
+        name: user.username,
+        email: user.email,
+        image: user.profilePic,
+        token,
+      });
+    } else {
+      res.status(400).json("Couldnt find the user");
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+const googleLogin = async (req, res) => {
+  try {
+    const token = req.body.credentialResponse.credential;
+    const decode = jwt.decode(token);
+    const { email } = decode;
+    const user = await usermodel.findOne({ email });
+    if (user) {
+      const token = generateToken(res, user._id);
+      res.status(200).json({
+        id: user._id,
+        name: user.username,
+        email: user.email,
+        image: user.profilePic,
+        token,
+      });
+    } else {
+      res.status(400).json("Invalid User");
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  check,
+  forgotpassword,
+  verifyOtp,
+  resetPassword,
+  googleAuth,
+  googleLogin,
+};
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,319 +1,11 @@
-import asyncHandler from "express-async-handler";
 import usermodel from "../modals/userModal.js";
-import generateToken from "../utils/userJwt.js";
-import sendresetmail from "../utils/nodeMailer.js";
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
 import postModel from "../modals/postModel.js";
-import fs from "fs";
-import mongoose from "mongoose";
 
-const registerUser = asyncHandler(async (req, res) => {
-  try {
-    const { username, email, phone, password } = req.body;
-    let userExist = await usermodel.findOne({ email });
-    let usernameExist = await usermodel.findOne({ username });
-    if (userExist) {
-      res.status(400).json("Email already exists");
-    }
-    if (usernameExist) {
-      res.status(400).json("Please Use unique usename");
-    }
-    const user = await usermodel.create({
-      username,
-      email,
-      phone,
-      password,
-      profilePic: "file_1695748280782.png",
-    });
-    if (user) {
-      let token = generateToken(res, user._id);
-      res.status(200).json({
-        id: user._id,
-        name: user.username,
-        email: user.email,
-        image: user.profilePic,
-        token,
-      });
-    }
-  } catch (err) {
-    res.status(400).json({ err: "Email already exist" });
-  }
-});
-
-const loginUser = async (req, res) => {
-  try {
-    let { email, password } = req.body;
-    let user = await usermodel.findOne({ email });
-    if (user) {
-      if (await user.matchpassword(password)) {
-        if (user.isBlocked) {
-          res.status(400).json({ error: "User is blocked" });
-        } else {
-          let token = generateToken(res, user._id);
-          res.status(200).json({
-            id: user._id,
-            name: user.username,
-            email: user.email,
-            image: user.profilePic,
-            token,
-          });
-        }
-      } else {
-        res.status(400).json({ error: "Wrong password" });
-      }
-    } else {
-      res.status(400).json({ error: "Wrong email" });
-    }
-  } catch (err) {
-    res.status(400).json({ error: "invalid " });
-  }
-};
-
-const check = (req, res) => {
-  res.status(200).json("Its workingggggggggggggggg");
-};
-
-const forgotpassword = async (req, res) => {
-  try {
-    let { email } = req.body;
-    let user = await usermodel.findOne({ email });
-    const token1 = Math.floor(100000 + Math.random() * 900000).toString();
-    const otpExpiration = new Date(Date.now() + 1 * 120 * 1000);
-    if (user) {
-      user.token = token1;
-      user.otpExpiration = otpExpiration;
-      await user.save();
-      sendresetmail(user.username, email, user.token);
-      res.status(200).json("its working");
-    } else {
-      res.status(400).json("User not found");
-    }
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-};
-
-const verifyOtp = async (req, res) => {
-  try {
-    let { state, otp } = req.body;
-
-    let user = await usermodel.findOne({ email: state });
-    if (user.token == otp) {
-      const currentTime = new Date();
-      if (currentTime <= user.otpExpiration) {
-        res.status(200).json("otp is matched");
-      } else {
-        res.status(200).json("Otp expired");
-      }
-    } else {
-      res.status(400).json("Otp not matched");
-    }
-  } catch (err) {
-    res.status(400).json({ error });
-  }
-};
-
-const resetPassword = async (req, res) => {
-  try {
-    let { state, password } = req.body;
-    let user = await usermodel.findOne({ email: state });
-    user.password = password;
-    await user.save();
-    res.status(200).json("reset password is working");
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-};
-
-const googleAuth = async (req, res) => {
-  try {
-    let token = req.body.credentialResponse.credential;
-    let decode = jwt.decode(token);
-    const { name, email, sub } = decode;
-    const userExists = await usermodel.findOne({ email });
-    if (userExists) {
-      res.status(400).json({ error: "User already exists" });
-    }
-
-    let username = "";
-    for (let i = 0; i < email.length; i++) {
-      if (email[i] == "@") {
-        break;
-      } else {
-        username += email[i];
-      }
-    }
-
-    const user = await usermodel.create({
-      username: username,
-      name: name,
-      email,
-      sub,
-      profilePic: "file_1695748280782.png",
-    });
-    if (user) {
-      let token = generateToken(res, user._id);
-      res.status(200).json({
-        id: user._id,
-        name: user.username,
-        email: user.email,
-        image: user.profilePic,
-        token,
-      });
-    } else {
-      res.status(400).json("Couldnt find the user");
-    }
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-};
-
-const googleLogin = async (req, res) => {
-  try {
-    let token = req.body.credentialResponse.credential;
-    let decode = jwt.decode(token);
-    const { email } = decode;
-    const user = await usermodel.findOne({ email });
-    if (user) {
-      let token = generateToken(res, user._id);
-      res.status(200).json({
-        id: user._id,
-        name: user.username,
-        email: user.email,
-        image: user.profilePic,
-        token,
-      });
-    } else {
-      res.status(400).json("Invalid User");
-    }
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
-
-// const logout = (req, res) => {
-//   console.log("haai");
-//   res.cookie("jwt", "", {
-//     httpOnly: true,
-//     expires: new Date(0),
-//   });
-//   res.status(200).json("logut success");
-// };
-
-const uploadPost = async (req, res) => {
-  try {
-    let { _id, heading, description, service } = req.body;
-
-    let currentDate = new Date();
-
-    let post = await postModel.create({
-      userId: _id,
-      heading: heading,
-      description: description,
-      service: service,
-      dateOfPosted: currentDate,
-    });
-    if (req.files) {
-      let uploadedFiles = req.files;
-      let fileUrls = [];
-      for (let file of uploadedFiles) {
-        const filePath = file.filename;
-        fileUrls.push(filePath);
-      }
-      post.media = fileUrls;
-      await post.save();
-    }
-    res.status(200).json({ heading: req.body.heading });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-};
-
-const editPost = async (req, res) => {
-  try {
-    let post = await postModel.findOne({ _id: req.query.id });
-    post.heading = req.body.heading;
-    post.description = req.body.description;
-    post.service = req.body.service;
-    await post.save();
-    console.log(post);
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-};
-
-const deletePost=async(req,res)=>{
-  try {
-    console.log(req.query.id)
-    let postDelete=await postModel.deleteOne({_id:req.query.id})
-    console.log(postDelete)
-    res.status(200).json('successfulll')
-
-  } catch (error) {
-    res.status(400).json(error.message)
-  }
-}
-
-const getPostforHome = async (req, res) => {
-  try {
-    let { id } = req.query;
-    let user = await usermodel.findOne({ _id: id });
-    if (user) {
-      let posts = await postModel.aggregate([
-        {
-          $lookup: {
-            from: "users",
-            localField: "userId",
-            foreignField: "_id",
-            as: "user",
-          },
-        },
-        {
-          $match: {
-            $or: [
-              { userId: user._id, isBlocked: { $ne: true } }, // Include user's own posts that are not blocked
-              { "user._id": { $in: user.following }, isBlocked: { $ne: true } }, // Include posts from users the current user follows that are not blocked
-            ],
-          },
-        },
-        {
-          $project: {
-            _id: 1,
-            heading: 1,
-            description: 1,
-            service: 1,
-            dateOfPosted: {
-              $dateToString: {
-                format:  "%Y-%m-%d %H:%M",
-                date: "$dateOfPosted",
-                timezone: "Asia/Kolkata",
-              },
-            },
-            media: 1,
-            likes: 1,
-            comments: 1,
-            "user.username": 1,
-            "user.profilePic": 1,
-          },
-        },
-        {
-          $sort: { dateOfPosted: -1 },
-        },
-      ]);
-
-      res.status(200).json(posts);
-    }
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-};
 
 const uploadCoverPic = async (req, res) => {
   try {
-    let { id } = req.body;
-    let user = await usermodel.findOne({ _id: id });
+    const { id } = req.body;
+    const user = await usermodel.findOne({ _id: id });
     if (user) {
       user.coverPic = req.file.filename;
       await user.save();
@@ -328,11 +20,11 @@ const uploadCoverPic = async (req, res) => {
 
 const getUserProfile = async (req, res) => {
   try {
-    let { id } = req.query;
+    const  { id } = req.query;
 
-    let user = await usermodel.findOne({ _id: id });
+    let  user = await usermodel.findOne({ _id: id });
     if (user) {
-      let post = await postModel.aggregate([
+      let  post = await postModel.aggregate([
         {
           $match: { userId: user._id }, // Filter posts by userId
         },
@@ -426,7 +118,7 @@ const getUserProfile = async (req, res) => {
 const otherUserProfile = async (req, res) => {
   try {
     let profile = await usermodel.findOne({ username: req.query.username });
-    let post = await postModel.aggregate([
+    let  post = await postModel.aggregate([
       {
         $match: { userId: profile._id }, // Filter posts by userId
       },
@@ -508,8 +200,8 @@ const otherUserProfile = async (req, res) => {
 
 const editProfile = async (req, res) => {
   try {
-    let { id, username, Phone, email, AboutUs, name } = req.body;
-    let user = await usermodel.findOne({ _id: id });
+    const { id, username, Phone, email, AboutUs, name } = req.body;
+    const user = await usermodel.findOne({ _id: id });
     if (user) {
       // user.profilePic = req.file.filename;
       user.username = username;
@@ -530,108 +222,9 @@ const editProfile = async (req, res) => {
   }
 };
 
-const postlike = async (req, res) => {
-  try {
-    let { id, userId } = req.query;
-    let post = await postModel.findOne({ _id: id });
-    const indexOfLike = post.likes.findIndex(
-      (like) => like.userId.toString() === userId
-    );
-
-    if (indexOfLike !== -1) {
-      // User has already liked the post, so remove the like
-      post.likes.splice(indexOfLike, 1);
-      await post.save();
-      let already = true;
-      res.status(200).json("unliked");
-    } else {
-      // User has not liked the post, so add a new like
-      post.likes.push({ userId: userId, timeStamp: new Date() });
-      await post.save();
-      res.status(200).json("liked");
-    }
-  } catch (err) {
-    res.status(400).json(err.message);
-  }
-};
-
-const postLikedUser = async (req, res) => {
-  try {
-    const postLike = await postModel.findOne({ _id: req.query.id });
-    if (!postLike) {
-      res.status(404).json({ message: "Post not found" });
-      return;
-    }
-    const userIds = postLike.likes.map((like) => like.userId);
-    const likedUsers = await usermodel.find({ _id: { $in: userIds } });
-    const users = likedUsers.map((user) => ({
-      username: user.username,
-      profilePic: user.profilePic,
-    }));
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-};
-
-const postCommentedUser = async (req, res) => {
-  try {
-    const postComment = await postModel.findOne({ _id: req.query.id });
-    if (!postComment) {
-      res.status(404).json({ message: "Post not found" });
-      return;
-    }
-
-    const comments = postComment.comments;
-    const userIds = comments.map((comment) => comment.userId);
-
-    const commentedUsers = await usermodel.find({ _id: { $in: userIds } });
-
-    const usersWithComments = comments.map((comment) => {
-      const user = commentedUsers.find((u) => u._id.equals(comment.userId));
-      return {
-        id:comment._id,
-        username: user.username,
-        profilePic: user.profilePic,
-        comment: comment.comment,
-      };
-    });
-    res.status(200).json(usersWithComments);
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-};
-
-const postComment = async (req, res) => {
-  try {
-    let { id, userId } = req.query;
-    let { typecomment } = req.body;
-    let post = await postModel.findOne({ _id: id });
-    post.comments.push({
-      userId: userId,
-      comment: typecomment,
-      timeStamp: new Date(),
-    });
-    await post.save();
-    res.status(200).json("commented");
-  } catch (err) {
-    res.status(400).json(err.message);
-  }
-};
-
-const commentDelete=async(req,res)=>{
-  try {
-    const comment=await postModel.updateOne({_id:req.query.postId},{ $pull: { comments: { _id:req.query.id } } })
-    
-    res.status(200).json('successfully deleted')
-  } catch (error) {
-    res.status(400).json(error.message)
-  }
-}
-
 const followManagement = async (req, res) => {
   try {
-    let { id, userId } = req.query;
+    const { id, userId } = req.query;
 
     let user = await usermodel.findOne({ username: id });
     let ourUser = await usermodel.findOne({ _id: userId });
@@ -692,7 +285,7 @@ const followManagement = async (req, res) => {
 
 const isBlocked = async (req, res) => {
   try {
-    let user = await usermodel.findOne({ _id: req.user._id });
+    const user = await usermodel.findOne({ _id: req.user._id });
     if (user) {
       next();
     } else {
@@ -705,10 +298,10 @@ const isBlocked = async (req, res) => {
 
 const search = async (req, res) => {
   try {
-    let users = await usermodel.find({
+    const users = await usermodel.find({
       username: { $regex: req.body.val, $options: "i" },
     });
-    let posts = await postModel.aggregate([
+    const posts = await postModel.aggregate([
       {
         $match: {
           isBlocked: { $ne: true },
@@ -754,7 +347,7 @@ const search = async (req, res) => {
         $sort: { dateOfPosted: -1 },
       },
     ]);
-    let service = await postModel.aggregate([
+    const service = await postModel.aggregate([
       {
         $match: {
           isBlocked: { $ne: true },
@@ -807,29 +400,11 @@ const search = async (req, res) => {
 };
 
 export {
-  registerUser,
-  loginUser,
-  check,
-  forgotpassword,
-  verifyOtp,
-  resetPassword,
-  googleAuth,
-  googleLogin,
-  // logout,
-  uploadPost,
-  editPost,
-  deletePost,
-  getPostforHome,
   uploadCoverPic,
   getUserProfile,
   editProfile,
-  postlike,
   otherUserProfile,
   followManagement,
-  postComment,
   isBlocked,
-  postLikedUser,
-  postCommentedUser,
-  commentDelete,
   search,
 };
